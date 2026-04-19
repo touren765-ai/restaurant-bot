275e79c8f2a626b56873aa9ecad7465396f8bac3
Ansou <touren765@gmail.com>
2026-04-19 04:40:14 +0200

    systeme multi-restaurants

--- menu.js
+++ menu.js
@@ -1,39 +1,42 @@
-var MENU = {
-  plats: [
-    { id: 1, nom: "Riz sauce arachide", prix: 15000 },
-    { id: 2, nom: "Thieboudiene", prix: 18000 },
-    { id: 3, nom: "Poulet yassa", prix: 20000 },
-    { id: 4, nom: "Brochettes de boeuf", prix: 12000 },
-    { id: 5, nom: "Attieke poisson", prix: 16000 }
-  ],
-  boissons: [
-    { id: 6, nom: "Jus bissap", prix: 3000 },
-    { id: 7, nom: "Jus gingembre", prix: 3000 },
-    { id: 8, nom: "Eau minerale", prix: 2000 }
-  ]
-};
+var fs = require('fs');
+var path = require('path');
 
-function afficherMenu() {
-  var texte = "Menu du Restaurant Le Baobab\n\n";
-  texte += "PLATS\n";
-  for (var i = 0; i < MENU.plats.length; i++) {
-    texte += MENU.plats[i].id + ". " + MENU.plats[i].nom + " - " + MENU.plats[i].prix + " GNF\n";
+function chargerRestaurant(id) {
+  try {
+    var fichier = path.join(__dirname, 'restaurants', id + '.json');
+    var data = fs.readFileSync(fichier, 'utf8');
+    return JSON.parse(data);
+  } catch(e) {
+    return null;
   }
-  texte += "\nBOISSONS\n";
-  for (var j = 0; j < MENU.boissons.length; j++) {
-    texte += MENU.boissons[j].id + ". " + MENU.boissons[j].nom + " - " + MENU.boissons[j].prix + " GNF\n";
+}
+
+function afficherMenu(restaurantId) {
+  var restaurant = chargerRestaurant(restaurantId || 'baobab');
+  if (!restaurant) return 'Restaurant non trouve.';
+  
+  var texte = 'Menu de ' + restaurant.nom + '\n\n';
+  texte += 'PLATS\n';
+  for (var i = 0; i < restaurant.plats.length; i++) {
+    texte += restaurant.plats[i].id + '. ' + restaurant.plats[i].nom + ' - ' + restaurant.plats[i].prix + ' GNF\n';
   }
-  texte += "\nTapez le numero du plat pour commander";
+  texte += '\nBOISSONS\n';
+  for (var j = 0; j < restaurant.boissons.length; j++) {
+    texte += restaurant.boissons[j].id + '. ' + restaurant.boissons[j].nom + ' - ' + restaurant.boissons[j].prix + ' GNF\n';
+  }
+  texte += '\nTapez le numero du plat pour commander';
   return texte;
 }
 
-function trouverPlat(numero) {
+function trouverPlat(numero, restaurantId) {
+  var restaurant = chargerRestaurant(restaurantId || 'baobab');
+  if (!restaurant) return null;
   var id = parseInt(numero);
-  var tous = MENU.plats.concat(MENU.boissons);
+  var tous = restaurant.plats.concat(restaurant.boissons);
   for (var i = 0; i < tous.length; i++) {
     if (tous[i].id === id) return tous[i];
   }
   return null;
 }
 
-module.exports = { MENU: MENU, afficherMenu: afficherMenu, trouverPlat: trouverPlat };
+module.exports = { afficherMenu: afficherMenu, trouverPlat: trouverPlat, chargerRestaurant: chargerRestaurant };
