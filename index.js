a79cb159022bc7e66f573c1793da85263263eb8d
Ansou <touren765@gmail.com>
2026-04-19 04:30:37 +0200

    ajout dashbord et railway

--- index.js
+++ index.js
@@ -33,7 +33,34 @@ app.post('/webhook', async function(req, res) {
 app.get('/', function(req, res) {
   res.send('Bot Restaurant Le Baobab - En ligne !');
 });
+app.get('/dashboard', function(req, res) {
+  res.sendFile(__dirname + '/dashboard.html');
+});
 
+app.get('/commandes', function(req, res) {
+  var db = require('./database');
+  db.commandesDuJour().then(function(commandes) {
+    res.json(commandes);
+  });
+});
+
+app.post('/commandes/:id/statut', function(req, res) {
+  var id = parseInt(req.params.id);
+  var statut = req.body.statut;
+  var fs = require('fs');
+  var path = require('path');
+  var fichier = path.join(__dirname, 'commandes.json');
+  var commandes = [];
+  try {
+    commandes = JSON.parse(fs.readFileSync(fichier, 'utf8'));
+  } catch(e) {}
+  commandes = commandes.map(function(c) {
+    if (c.id === id) c.statut = statut;
+    return c;
+  });
+  fs.writeFileSync(fichier, JSON.stringify(commandes, null, 2));
+  res.json({ ok: true });
+});
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log('Serveur demarre sur le port ' + PORT);
