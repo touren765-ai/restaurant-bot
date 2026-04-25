7d5ae09c7f80d80f71b9c3e305271b5cb75a49dc
Ansou <touren765@gmail.com>
2026-04-25 04:22:59 +0200

    ajout page abonnement

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 require('dotenv').config();
 var express = require('express');
 var bot = require('./bot');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/webhook', async function(req, res) {
-  var telephone = req.body.From || req.body.from || '';
-  var message = req.body.Body || req.body.body || '';
-
+  var telephone = req.body.From || '';
+  var message = req.body.Body || '';
   console.log('Body recu:', JSON.stringify(req.body));
   console.log('Nouveau message de ' + telephone + ': ' + message);
-
   if (!message) {
     res.type('text/xml');
     res.send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
     return;
   }
-
   try {
     var reponse = await bot.traiterMessage(telephone, message);
     var twiml = '<?xml version="1.0" encoding="UTF-8"?><Response><Message>' + reponse + '</Message></Response>';
@@ -30,11 +29,31 @@ app.post('/webhook', async function(req, res) {
   }
 });
 
-app.get('/', function(req, res) {
-  res.send('Bot Restaurant Le Baobab - En ligne !');
-});
 app.get('/dashboard', function(req, res) {
-  res.sendFile(__dirname + '/dashboard.html');
+  res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+
+app.get('/abonnement', function(req, res) {
+  res.sendFile(path.join(__dirname, 'abonnement.html'));
+});
+
+app.post('/abonnement', function(req, res) {
+  var demande = {
+    id: Date.now(),
+    plan: req.body.plan,
+    nom: req.body.nom,
+    telephone: req.body.telephone,
+    telephonePaiement: req.body.telephonePaiement,
+    date: new Date(),
+    statut: 'en_attente'
+  };
+  var fichier = path.join(__dirname, 'abonnements.json');
+  var abonnements = [];
+  try { abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
+  abonnements.push(demande);
+  fs.writeFileSync(fichier, JSON.stringify(abonnements, null, 2));
+  console.log('Nouvelle demande:', demande.nom, '-', demande.plan);
+  res.json({ ok: true });
 });
 
 app.get('/commandes', function(req, res) {
@@ -47,13 +66,9 @@ app.get('/commandes', function(req, res) {
 app.post('/commandes/:id/statut', function(req, res) {
   var id = parseInt(req.params.id);
   var statut = req.body.statut;
-  var fs = require('fs');
-  var path = require('path');
   var fichier = path.join(__dirname, 'commandes.json');
   var commandes = [];
-  try {
-    commandes = JSON.parse(fs.readFileSync(fichier, 'utf8'));
-  } catch(e) {}
+  try { commandes = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
   commandes = commandes.map(function(c) {
     if (c.id === id) c.statut = statut;
     return c;
@@ -61,6 +76,11 @@ app.post('/commandes/:id/statut', function(req, res) {
   fs.writeFileSync(fichier, JSON.stringify(commandes, null, 2));
   res.json({ ok: true });
 });
+
+app.get('/', function(req, res) {
+  res.send('Bot Restaurant Le Baobab - En ligne !');
+});
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log('Serveur demarre sur le port ' + PORT);
