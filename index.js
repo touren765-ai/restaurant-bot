2ac106a7ed9aaab624fabdecd3418f4466ed8512
Ansou <touren765@gmail.com>
2026-06-03 11:51:00 +0000

    dashboard RH BCEIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+ require('dotenv').config();
 var express = require('express');
 var bot = require('./bot');
 var bot_bceip = require('./bot_bceip');
@@ -27,82 +27,6 @@ app.use(express.json());app.post('/webhook', async function(req, res) {
   }
 });
 
-app.get('/dashboard', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dashboard.html'));
-});
-
-app.get('/abonnement', function(req, res) {
-  res.sendFile(path.join(__dirname, 'abonnement.html'));
-});
-
-app.get('/admin', function(req, res) {
-  res.sendFile(path.join(__dirname, 'admin.html'));
-});app.post('/abonnement', function(req, res) {
-  var demande = {
-    id: Date.now(),
-    plan: req.body.plan,
-    nom: req.body.nom,
-    telephone: req.body.telephone,
-    telephonePaiement: req.body.telephonePaiement,
-    date: new Date(),
-    statut: 'en_attente'
-  };
-  var fichier = path.join(__dirname, 'abonnements.json');
-  var abonnements = [];
-  try {
-    abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8'));
-  } catch(e) {}
-  abonnements.push(demande);
-  fs.writeFileSync(fichier, JSON.stringify(abonnements, null, 2));
-  console.log('Nouvelle demande:', demande.nom, '-', demande.plan);
-  res.json({ ok: true });
-});
-
-app.get('/admin/demandes', function(req, res) {
-  var fichier = path.join(__dirname, 'abonnements.json');
-  var abonnements = [];
-  try {
-    abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8'));
-  } catch(e) {}
-  res.json(abonnements);
-});
-
-app.post('/admin/demandes/:id', function(req, res) {
-  var id = parseInt(req.params.id);
-  var statut = req.body.statut;
-  var fichier = path.join(__dirname, 'abonnements.json');
-  var abonnements = [];
-  try {
-    abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8'));
-  } catch(e) {}
-  abonnements = abonnements.map(function(a) {
-    if (a.id === id) a.statut = statut;
-    return a;
-  });
-  fs.writeFileSync(fichier, JSON.stringify(abonnements, null, 2));
-  res.json({ ok: true });
-});app.get('/commandes', function(req, res) {
-  var db = require('./database');
-  db.commandesDuJour().then(function(commandes) {
-    res.json(commandes);
-  });
-});
-
-app.post('/commandes/:id/statut', function(req, res) {
-  var id = parseInt(req.params.id);
-  var statut = req.body.statut;
-  var fichier = path.join(__dirname, 'commandes.json');
-  var commandes = [];
-  try {
-    commandes = JSON.parse(fs.readFileSync(fichier, 'utf8'));
-  } catch(e) {}
-  commandes = commandes.map(function(c) {
-    if (c.id === id) c.statut = statut;
-    return c;
-  });
-  fs.writeFileSync(fichier, JSON.stringify(commandes, null, 2));
-  res.json({ ok: true });
-});
 app.post('/webhook/bceip', async function(req, res) {
   var telephone = req.body.From || '';
   var message = req.body.Body || '';
@@ -121,7 +45,81 @@ app.post('/webhook/bceip', async function(req, res) {
     console.error('Erreur BCEIP:', error);
     res.status(500).send('Erreur serveur');
   }
+});app.get('/dashboard', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
+
+app.get('/dashboard/bceip', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dashboard_bceip.html'));
+});
+
+app.get('/abonnement', function(req, res) {
+  res.sendFile(path.join(__dirname, 'abonnement.html'));
+});
+
+app.get('/admin', function(req, res) {
+  res.sendFile(path.join(__dirname, 'admin.html'));
+});
+
+app.post('/abonnement', function(req, res) {
+  var demande = {
+    id: Date.now(),
+    plan: req.body.plan,
+    nom: req.body.nom,
+    telephone: req.body.telephone,
+    telephonePaiement: req.body.telephonePaiement,
+    date: new Date(),
+    statut: 'en_attente'
+  };
+  var fichier = path.join(__dirname, 'abonnements.json');
+  var abonnements = [];
+  try { abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
+  abonnements.push(demande);
+  fs.writeFileSync(fichier, JSON.stringify(abonnements, null, 2));
+  console.log('Nouvelle demande:', demande.nom, '-', demande.plan);
+  res.json({ ok: true });
+});
+
+app.get('/admin/demandes', function(req, res) {
+  var fichier = path.join(__dirname, 'abonnements.json');
+  var abonnements = [];
+  try { abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
+  res.json(abonnements);
+});
+
+app.post('/admin/demandes/:id', function(req, res) {
+  var id = parseInt(req.params.id);
+  var statut = req.body.statut;
+  var fichier = path.join(__dirname, 'abonnements.json');
+  var abonnements = [];
+  try { abonnements = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
+  abonnements = abonnements.map(function(a) {
+    if (a.id === id) a.statut = statut;
+    return a;
+  });
+  fs.writeFileSync(fichier, JSON.stringify(abonnements, null, 2));
+  res.json({ ok: true });
+});app.get('/commandes', function(req, res) {
+  var db = require('./database');
+  db.commandesDuJour().then(function(commandes) {
+    res.json(commandes);
+  });
+});
+
+app.post('/commandes/:id/statut', function(req, res) {
+  var id = parseInt(req.params.id);
+  var statut = req.body.statut;
+  var fichier = path.join(__dirname, 'commandes.json');
+  var commandes = [];
+  try { commandes = JSON.parse(fs.readFileSync(fichier, 'utf8')); } catch(e) {}
+  commandes = commandes.map(function(c) {
+    if (c.id === id) c.statut = statut;
+    return c;
+  });
+  fs.writeFileSync(fichier, JSON.stringify(commandes, null, 2));
+  res.json({ ok: true });
+});
+
 app.get('/', function(req, res) {
   res.send('Bot Restaurant Le Baobab - En ligne !');
 });
